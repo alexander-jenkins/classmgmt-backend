d30463b8b23b2eddc6044ea29f5882bff16929cb
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2022-04-20 17:26:54 -0400

    connected NestJS app to postgres database

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## Description
 
 Backend to connect to the Postgress database for the Class Management system project for the Church of Philippi in COSC 412.
+
+## Members
+
+- Alexander Jenkins
+- Alecia Rotunno
+- Dushaw Hockett
+- Emma Camelo
+- Neil Labio
+- Jacob Weagly
---
+++ src/orm.config.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+require('dotenv').config();
+
+// parse the .env file for the database connection
+const db_host = process.env.POSTGRES_HOST || 'localhost';
+const db_port = parseInt(process.env.POSTGRES_PORT) || 5432;
+const conn_db = process.env.POSTGRES_DB || 'postgres';
+const sync_db = process.env.SYNC_DB == 'true' || false;
+const db_user = process.env.POSTGRES_USER || 'postgres';
+const db_password = process.env.POSTGRES_PASSWORD || 'postgres';
+
+export const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  username: db_user,
+  password: db_password,
+  port: db_port,
+  host: db_host,
+  database: conn_db,
+  synchronize: sync_db,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+};
